57c6a354f570fe21a76a37bb8f59778e5232a211
Mirjam <mirjam.mclachlan@gmail.com>
2026-09-16 21:32:21 +0200

    TodoApp shell

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import TodoApp from './components/TodoApp.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <TodoApp />
 </template>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="todo-app">
+    <h1>Todo App</h1>
+  </div>
+</template>
